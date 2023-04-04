5290645d01d412ff429291c3d8a6d8ad0db97bb8
rdabadie <raphael.dabadie@ensae.fr>
2023-04-04 18:15:33 +0000

    Mardi

--- tests/test_s1q5_min_distance.py
+++ tests/test_s1q5_min_distance.py
@@ -7,14 +7,9 @@ import unittest   # The test framework
 
 class Test_Reachability(unittest.TestCase):
     def test_network0(self):
-        g = graph_from_file("input/network.00.in")
-        self.assertEqual(g.get_path_with_power_dijkstra(1, 4, 11), [1, 2, 3, 4])
-        self.assertEqual(g.get_path_with_power_dijkstra(1, 4, 10), None)
-
-    def test_network2(self):
-        g = graph_from_file("input/network.02.in")
-        self.assertIn(g.get_path_with_power_dijkstra(1, 2, 11), [[1, 2], [1, 4, 3, 2]])
-        self.assertEqual(g.get_path_with_power_dijkstra(1, 2, 5), [1, 4, 3, 2])
+        g = graph_from_file("input/network.04.in")
+        self.assertEqual(g.get_path_with_power_dijkstra(2, 3, 5), [2,3])
+        self.assertEqual(g.get_path_with_power_dijkstra(1, 3, 0), None)
 
 if __name__ == '__main__':
     unittest.main()
